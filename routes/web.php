155cfee5dce430c63b2581c26e87c4a097361bcb
kai <cgk0910@gmail.com>
2023-09-04 13:19:18 +0800

    teacher controller

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\TeacherController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -23,3 +24,4 @@ use App\Http\Controllers\StudentController;
 
 
 Route::resource('students', StudentController::class);
+Route::resource('teachers', TeacherController::class);
